90312f3179348fe9def1b11581d173fd7e296b81
Robyy1 <pirvurobert36@gmail.com>
2022-02-12 21:32:13 +0200

    Day 2

---
+++ Youtube/Projects/Projects/Online courses/30 Days of python/Day 1.py
@@ -0,0 +1,44 @@
+#Strings
+
+#print(this is a word)
+print("this is a word")
+#print(String)
+print("another string is cool. another thing here")
+
+
+print("hi this is a " + "another string")
+
+
+
+#Variables
+
+#x + y = 10
+#b + c = 10
+
+abc = "some string"
+print("some string")
+print(abc)
+
+print(abc + abc)
+
+
+
+#Numbers
+
+#print(this is a word)
+print("this is a word")
+
+print(123)
+#print(123 is awesome)
+print("123 is awesome")
+print("123")
+
+print(2 * 2)
+print("2" * 2)
+print("abc" * 2)
+print("this is a word " * 2)
+print("this is word " * 10)
+
+print(2 * 10)
+print(9/4)
+print(9/4.0)
