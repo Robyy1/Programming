d4ab8db53202351fba7e88cc42bdc05d9c407ff0
Robyy1 <pirvurobert36@gmail.com>
2022-02-18 21:57:52 +0200

    clock

---
+++ Youtube/Projects/Projects/Online courses/30 Days of python/Day 4.py
@@ -0,0 +1,27 @@
+#Conditionals
+print('true')
+print('false')
+
+print(True==True)
+print("justin"=="Justin")
+print("justin"=="justin")
+
+obj_a = True
+obj_b = False
+obj_a == True
+
+name = "justin"
+name == True    
+
+list_a = [1,2,3]
+list_b = [1,2,3]
+list_c = [4,5,6]
+print(list_a==list_b)
+print(list_a==list_c)
+print(list_a!=list_c)
+
+print(not obj_a)
+print(obj_b)
+
+print(not obj_a == obj_b)
+print(not obj_a is obj_b)
